2653a2152abf713a38f0de45b97005b22078072f
dennisruppe <dennis.ruppe@gmail.com>
2022-04-05 14:53:15 -0400

    Creating Item Templates

--- scripts/module.js
+++ scripts/module.js
@@ -82,7 +82,7 @@ class CraftingConfigData {
                 default:
                     score = 1;
             }
-            ModularAlchemy.log(false, "Checking reagent effects", reagent);
+            
             for (const effectId of reagent.flags[ModularAlchemy.ID][ModularAlchemy.FLAGS.ITEMREAGENTEFFECTS]) {
                 const effectJournal = await game.packs.get(ModularAlchemy.SOURCE_EFFECTS).getDocument(effectId);
                 const maxlevel = effectJournal.getFlag(ModularAlchemy.ID, ModularAlchemy.FLAGS.EFFECTMAXLEVEL);
@@ -106,6 +106,132 @@ class CraftingConfigData {
         }
 
         return groupedEffects;
+    }
+
+    static romanNumeral(num) {
+        return ['I', 'II', 'III', 'IV', 'V'][num-1];
+    }
+
+    static consumableTypeMapping(type) {
+        const map = {
+            'Bomb': 'trinket',
+            'Poison': 'poison',
+            'Potion': 'potion'
+        }
+        return map[type];
+    }
+
+    static getDCForEffectLevel(level) {
+        const map = {
+            1: 10,
+            2: 13,
+            3: 15,
+            4: 17
+        }
+        return map[level];
+    }
+
+    static getAreaForEffectLevel(level) {
+        const map = {
+            1: 5,
+            2: 10,
+            3: 15,
+            4: 20
+        }
+        return map[level];
+    }
+
+    static getDurationForEffectLevel(level) {
+        const map = {
+            1: {value: 1, units: 'minute'},
+            2: {value: 10, units: 'minute'},
+            3: {value: 1, units: 'hour'},
+            4: {value: 5, units: 'hour'}
+        }
+        return map[level];
+    }
+
+    static getBombDamageForEffectLevel(level) {
+        const map = {
+            1: "1d6",
+            2: "3d6",
+            3: "5d6",
+            4: "7d6"
+        }
+        return map[level];
+    }
+
+    static getPoisonDamageForEffectLevel(level) {
+        const map = {
+            1: "1d6",
+            2: "3d6",
+            3: "5d6",
+            4: "6d6"
+        }
+        return map[level];
+    }
+
+    static getPotionHealingForEffectLevel(level) {
+        const map = {
+            1: "2d4+2",
+            2: "4d4+4",
+            3: "8d4+8",
+            4: "10d4+20"
+        }
+        return map[level];
+    }
+
+    static async createNewItem(type, effects) {
+        ModularAlchemy.log(false, "Crafting:", type, effects);
+
+        const highestLevel = Math.max(...effects.map(x => x.level));
+
+        const typeSpecificData = {data: {}};
+
+        switch (type) {
+            case 'Bomb':
+                typeSpecificData['data']['ability'] = 'dex';
+                typeSpecificData['data']['actionType'] = 'save';
+                typeSpecificData['data']['range'] = {value: 30, units: 'ft'};
+                typeSpecificData['data']['save'] = {ability: 'dex', dc: this.getDCForEffectLevel(highestLevel), scaling: 'flat'};
+                typeSpecificData['data']['target'] = {value: this.getAreaForEffectLevel(highestLevel), units: 'ft', type: 'radius'};
+                const bombDamages = effects.filter(x => x.name.startsWith('Bomb')).map(x => [this.getBombDamageForEffectLevel(x.level), /Bomb \((.*)\)/.exec(x.name)?.[1]?.toLowerCase()]);
+                typeSpecificData['data']['damage'] = {parts: bombDamages};
+                break;
+            case 'Poison':
+                typeSpecificData['data']['ability'] = 'con';
+                typeSpecificData['data']['actionType'] = 'save';
+                typeSpecificData['data']['save'] = {ability: 'con', dc: this.getDCForEffectLevel(highestLevel), scaling: 'flat'};
+                typeSpecificData['data']['duration'] = this.getDurationForEffectLevel(highestLevel);
+                const poisonDamages = effects.filter(x => x.name.startsWith('Harm')).map(x => [this.getPoisonDamageForEffectLevel(x.level), 'poison']);
+                typeSpecificData['data']['damage'] = {parts: poisonDamages};
+                break;
+            case 'Potion':
+                typeSpecificData['data']['actionType'] = 'heal';
+                typeSpecificData['data']['duration'] = this.getDurationForEffectLevel(highestLevel);
+                const potionHealing = effects.filter(x => x.name.startsWith('Healing')).map(x => [this.getPotionHealingForEffectLevel(x.level), 'healing']);
+                typeSpecificData['data']['damage'] = {parts: potionHealing};
+                break;    
+            default:
+                break;
+        }
+
+        const data = {
+            name: `Modular ${type}: [${effects.map(x=>x.name+' '+this.romanNumeral(x.level)).join(', ')}]`,
+            type: 'consumable',
+            data: {
+                consumableType: this.consumableTypeMapping(type),
+                activation: {cost: 1, type: 'action'},
+                uses: {value: 1, max: 1, autodestroy: false}
+            }
+        };
+
+        const mergedData = foundry.utils.mergeObject(data, typeSpecificData);
+        const craftedItem = await Item.create(mergedData);
+
+        // update with active effects: 
+
+        return craftedItem;
     }
 }
 
@@ -162,7 +288,27 @@ class CraftingConfig extends FormApplication {
                 this.render();
                 break;
             }
-
+            case 'craft Bomb': {
+                const effects = await CraftingConfigData.getEffectsForReagents(this.options.userId);
+                const relEffects = effects['Bomb'];
+                await CraftingConfigData.createNewItem('Bomb', relEffects);
+                this.render();
+                break;
+            }
+            case 'craft Poison': {
+                const effects = await CraftingConfigData.getEffectsForReagents(this.options.userId);
+                const relEffects = effects['Poison'];
+                await CraftingConfigData.createNewItem('Poison', relEffects);
+                this.render();
+                break;
+            }
+            case 'craft Potion': {
+                const effects = await CraftingConfigData.getEffectsForReagents(this.options.userId);
+                const relEffects = effects['Potion'];
+                await CraftingConfigData.createNewItem('Potion', relEffects);
+                this.render();
+                break;
+            }
             default:
                 ModularAlchemy.log(false, 'Invalid action detected', action);
         }
